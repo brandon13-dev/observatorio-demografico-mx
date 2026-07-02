804f91258ab9c514c141c5b597b36f33caaecde5
Brandon Arroyo <brandon.arroyo.2000@gmail.com>
2026-07-02 14:52:09 -0600

    chore: estructura inicial del proyecto Spring Boot

---
+++ README.md
@@ -0,0 +1,11 @@
+Observatorio Demográfico Municipal de México.
+
+Proyecto desarrollado utilizando:
+- Spring Boot 3.
+- Angular 19.
+- PostgreSQL
+- Docker.
+- API de INEGI
+
+Objetivo:
+Visualizar y analizar indicadores demográficos de municipios mexicanos mediante datos oficiales del INEGI.
---
+++ analizador-demografico-backend/src/main/java/mx/brandonarroyo/analizadordemografico/AnalizadorDemograficoApplication.java
@@ -0,0 +1,13 @@
+package mx.brandonarroyo.analizadordemografico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalizadorDemograficoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalizadorDemograficoApplication.class, args);
+	}
+
+}
